3c223d78bdd0963f6df26e80dd693f5930e50461
Danielle Horn <dch9530@br006.pvt.bridges.psc.edu>
2018-05-21 15:03:33 -0400

    message

--- toy_batch.sh
+++ toy_batch.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
 #SBATCH -N 1
 #SBATCH -p GPU
 #SBATCH --ntasks-per-node 28
 #SBATCH -t 00:10:00
-#SBATCH --gres=gpu:p100:1
+#SBATCH --gres=gpu:p100:2
+#SBATCH -A ac561qp
 
 module load python3
 
